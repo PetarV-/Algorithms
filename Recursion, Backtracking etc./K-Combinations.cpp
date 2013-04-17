da8fefada7a141501530f99f8120779c98ff6c53
Petar Veličković <pv273@cam.ac.uk>
2013-04-17 22:24:27 +0300

    Update and rename 1.in.txt to K-Combinations.cpp

---
+++ Recursion, Backtracking etc./K-Combinations.cpp
@@ -0,0 +1,50 @@
+/*
+ Petar 'PetarV' Velickovic
+ Algorithm: K-Combinations
+*/
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <complex>
+using namespace std;
+typedef long long lld;
+
+int n, k;
+int skup[100];
+
+//Program koji generise sve kombinacije od po K elemenata datog skupa
+//Slozenost O(2^n)
+
+void kCombinations(int pos, int amt)
+{
+     if (n-pos<k-amt) return;
+     if (amt==k)
+     {
+         for (int i=0;i<n;i++) if (inSet[i]) printf("%d ",skup[i]);
+         printf("\n");
+         return;
+     }
+     recurse(pos+1,amt+1);
+     recurse(pos+1,amt);
+}
+
+int main()
+{
+    n = 5, k = 2;
+    skup[0] = 1;
+    skup[1] = 2;
+    skup[2] = 3;
+    skup[3] = 4;
+    skup[4] = 5;
+    kCombinations(0, 0);
+    return 0;
+}
