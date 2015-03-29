52e82d6e8e459d1062490950644d7c082ae6ec71
Petar Veličković <pv273@cam.ac.uk>
2015-03-29 20:19:27 +0200

    Added Needleman-Wunsch

---
+++ Dynamic Programming/Needleman-Wunsch.cpp
@@ -0,0 +1,126 @@
+/*
+ Petar 'PetarV' Velickovic
+ Algorithm: Needleman-Wunsch
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <complex>
+
+#define MAX_N 50001
+#define LIMIT 1000000
+#define MID (left+right)/2
+
+#define DPRINTC(C) printf(#C " = %c\n", (C))
+#define DPRINTS(S) printf(#S " = %s\n", (S))
+#define DPRINTD(D) printf(#D " = %d\n", (D))
+#define DPRINTLLD(LLD) printf(#LLD " = %lld\n", (LLD))
+#define DPRINTLF(LF) printf(#LF " = %.5lf\n", (LF))
+
+using namespace std;
+typedef long long lld;
+typedef unsigned long long llu;
+
+#define MAX_N 1001
+using namespace std;
+typedef long long lld;
+
+int n, m;
+int match_score, mismatch_score, gap_score;
+string A, B;
+int dp[MAX_N][MAX_N];
+
+/*
+ Needleman-Wunsch algorithm for determining the optimal alignment between two strings
+ assuming a given score for hits, gaps and mismatches.
+ Complexity: O(n * m) time, O(n * m) memory
+*/
+
+inline int needleman_wunsch()
+{
+    for (int i=0;i<=n;i++) dp[i][0] = dp[0][i] = -i * gap_score;
+    for (int i=1;i<=n;i++)
+    {
+        for (int j=1;j<=m;j++)
+        {
+            int S = (A[i-1] == B[j-1]) ? match_score : -mismatch_score;
+            dp[i][j] = max(dp[i-1][j-1] + S, max(dp[i-1][j] - gap_score, dp[i][j-1] - gap_score));
+        }
+    }
+    return dp[n][m];
+}
+
+inline pair<string, string> get_optimal_alignment()
+{
+    string retA, retB;
+    stack<char> SA, SB;
+    int ii = n, jj = m;
+    while (ii != 0 || jj != 0)
+    {
+        if (ii == 0)
+        {
+            SA.push('-');
+            SB.push(B[jj-1]);
+            jj--;
+        }
+        else if (jj == 0)
+        {
+            SA.push(A[ii-1]);
+            SB.push('-');
+            ii--;
+        }
+        else
+        {
+            int S = (A[ii-1] == B[jj-1]) ? match_score : -mismatch_score;
+            if (dp[ii][jj] == dp[ii-1][jj-1] + S)
+            {
+                SA.push(A[ii-1]);
+                SB.push(B[jj-1]);
+                ii--; jj--;
+            }
+            else if (dp[ii-1][jj] > dp[ii][jj-1])
+            {
+                SA.push(A[ii-1]);
+                SB.push('-');
+                ii--;
+            }
+            else
+            {
+                SA.push('-');
+                SB.push(B[jj-1]);
+                jj--;
+            }
+        }
+    }
+    while (!SA.empty())
+    {
+        retA += SA.top();
+        retB += SB.top();
+        SA.pop();
+        SB.pop();
+    }
+    return make_pair(retA, retB);
+}
+
+int main()
+{
+    n = 5, m = 6;
+    match_score = 2, mismatch_score = 1, gap_score = 1;
+    A = "CATGT";
+    B = "ACGCTG";
+    printf("%d\n",needleman_wunsch());
+    pair<string, string> alignment = get_optimal_alignment();
+    printf("%s\n%s\n", alignment.first.c_str(), alignment.second.c_str());
+    return 0;
+}
